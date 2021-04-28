879c1452fb8ba279512bdbf00275536e8a0a9a2d
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-04-28 09:47:15 -0300

    apply fake api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home(props) {
   return (
     <h1>index</h1>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8,
+  }
 }
