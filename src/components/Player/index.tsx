855487e75636f1d0ef8e31c27d1d731d877be020
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-05-02 20:25:20 -0300

    add next and previous function

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
-import { useContext, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import { PlayerContext } from '../../contexts/PlayerContext';
+import { usePlayer } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export function Player() {
@@ -11,9 +11,16 @@ export function Player() {
   const { 
     episodeList, 
     currentEpisodeIndex, 
-    isPlaying, 
+    isPlaying,
+    isLooping, 
+    toggleLoop,
     togglePlay,
-    setPlayingState } = useContext(PlayerContext);
+    setPlayingState,
+    playNext,
+    playPrevious,
+    hasNext,
+    hasPrevious
+  } = usePlayer();
 
   useEffect(() => {
     if (!audioRef.current) {
@@ -75,6 +82,7 @@ export function Player() {
             ref={audioRef}
             src={episode.url} 
             autoPlay
+            loop={isLooping}
             onPlay={() => setPlayingState(true)}
               onPause={() => setPlayingState(false)}
           />
@@ -84,7 +92,7 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="shuffle.svg" alt="Embaralhar"/>
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button" onClick={playPrevious} disabled={!episode || !hasPrevious}>
             <img src="play-previous.svg" alt="Tocar anterior"/>
           </button>
           <button 
@@ -99,11 +107,16 @@ export function Player() {
               <img src="play.svg" alt="Tocar"/>
             ) }
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button" onClick={playNext} disabled={!episode || !hasNext}>
             <img src="play-next.svg" alt="Tocar próxima"/>
           </button>
           <button type="button" disabled={!episode}>
-            <img src="repeat.svg" alt="Repetir"/>
+            <img 
+              src="repeat.svg" 
+              alt="Repetir" 
+              onClick={toggleLoop}
+              className={isLooping ? styles.isActive : ""}
+            />
           </button>
         </div>
       </footer>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,7 @@ import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss';
-import { useContext } from 'react';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { usePlayer } from '../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -27,14 +26,16 @@ type HomeProps = {
 }
 
 export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
-  const { play } = useContext(PlayerContext);
+  const { playList } = usePlayer();
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
         <ul>
-          {latestEpisodes.map(episode => {
+          {latestEpisodes.map((episode, index) => {
             return (
               <li key={episode.id}>
                 <Image
@@ -53,7 +54,7 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button" onClick={() => play(episode)}>
+                <button type="button" onClick={() => playList(episodeList, index)}>
                   <img src="/play-green.svg" alt="Tocar Episódio"/>
                 </button>
               </li>
@@ -77,7 +78,7 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
             </tr>
           </thead>
           <tbody>
-            {allEpisodes.map(episode => {
+            {allEpisodes.map((episode, index) => {
               return (
                 <tr key={episode.id}>
                   <td style={{ width: 72 }}>
@@ -98,7 +99,7 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button">
+                    <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
                       <img src="/play-green.svg" alt="Tocar episódio"/>
                     </button>
                   </td>
